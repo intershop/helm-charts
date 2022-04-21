3add08707a8b3e191ddec2c3fdd4e1ccb0296ecb
Karsten Ludwig Hauser <KHauser@intershop.com>
2022-04-21 11:57:25 +0200

    #12: also add icm-replication to bum version support

--- .github/callBump2version.py
+++ .github/callBump2version.py
@@ -4,27 +4,28 @@ from enum import EnumMeta, Enum
 
 class MyEnumMeta(EnumMeta):
 
-	def __contains__(self, other):
-		try:
-			self(other)
-		except ValueError:
-			return False
-		else:
-			return True
+    def __contains__(self, other):
+        try:
+            self(other)
+        except ValueError:
+            return False
+        else:
+            return True
 
 class UpgradeType(Enum, metaclass=MyEnumMeta):
-	NONE = 'none'
-	PATCH = 'patch'
-	MINOR = 'minor'
-	MAJOR = 'major'
+    NONE = 'none'
+    PATCH = 'patch'
+    MINOR = 'minor'
+    MAJOR = 'major'
 
 class ProductType(Enum, metaclass=MyEnumMeta):
-	NONE = 'none'
-	IOM = 'iom'
-	PWA = 'pwa'
-	ICM = 'icm'
-	ICM_AS = 'icm-as'
-	ICM_WEB = 'icm-web'
+    NONE = 'none'
+    IOM = 'iom'
+    PWA = 'pwa'
+    ICM = 'icm-repliciation'
+    ICM = 'icm'
+    ICM_AS = 'icm-as'
+    ICM_WEB = 'icm-web'
 
 def handle_subprocess_error(subprocess_result, error_message):
     if subprocess_result.returncode != 0:
@@ -37,28 +38,28 @@ def handle_subprocess_error(subprocess_result, error_message):
         exit(1)
 
 def main(argv):
-	chart = ProductType.NONE
-	upgrade = UpgradeType.NONE
+    chart = ProductType.NONE
+    upgrade = UpgradeType.NONE
 
-	for label in argv:
-		if label in ProductType:
-			chart = ProductType(label)
+    for label in argv:
+        if label in ProductType:
+            chart = ProductType(label)
 
-	for label in argv:
-		if label in UpgradeType:
-			upgrade = UpgradeType(label)
+    for label in argv:
+        if label in UpgradeType:
+            upgrade = UpgradeType(label)
 
-	if upgrade == UpgradeType.NONE or chart == ProductType.NONE:
-		print("No need for a version bump detected")
-		exit(0)
+    if upgrade == UpgradeType.NONE or chart == ProductType.NONE:
+        print("No need for a version bump detected")
+        exit(0)
 
-	print(f"Bumping version of \'{chart.value}\' to next \'{upgrade.value}\' value.")
-	bumpver_process = subprocess.run(f"bump2version {upgrade.value}",
+    print(f"Bumping version of \'{chart.value}\' to next \'{upgrade.value}\' value.")
+    bumpver_process = subprocess.run(f"bump2version {upgrade.value}",
                                      shell=True,
                                      cwd=f"./charts/{chart.value}",
                                      capture_output=True)
-	handle_subprocess_error(bumpver_process, "Could not execute version bump.")
+    handle_subprocess_error(bumpver_process, "Could not execute version bump.")
 
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
