ab738347265c616949f3c8214ba492bf7be979cf
Karsten Ludwig Hauser <KHauser@intershop.com>
2023-02-09 12:38:11 +0100

    fix (icm): copy sites folder for testsuite (#195)

--- charts/icm/start-test-local.sh
+++ charts/icm/start-test-local.sh
@@ -6,7 +6,7 @@ source start-test-local_vars.sh
 
 # copy values_iste.tmpl to values.yaml and exchange all declared variables
 set +u
-eval "echo \"$(< values-iste_linux.tmpl)\"" > values-test-local.tmp
+eval "echo \"$(< values-test-local.yaml)\"" > values-test-local.tmp
 set -u
 
-helm --kubeconfig ${KUBECONFIG} install ${HELM_DRY_RUN}${HELM_JOB_NAME} . -f ./values-test-local.tmp -f ./values-test-local.yaml
+helm --kubeconfig ${KUBECONFIG} upgrade --install ${HELM_DRY_RUN}${HELM_JOB_NAME} . -f ./values-test-local.tmp
