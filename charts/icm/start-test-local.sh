b8cc608fa545613c9895ecd7e7d301084bf84316
Karsten Ludwig Hauser <KHauser@intershop.com>
2023-02-09 14:04:08 +0100

    fix (icm): use env replacement plugin for helm (#195)

--- charts/icm/start-test-local.sh
+++ charts/icm/start-test-local.sh
@@ -4,9 +4,8 @@ set -eu pipefail
 
 source start-test-local_vars.sh
 
-# copy values_iste.tmpl to values.yaml and exchange all declared variables
-set +u
-eval "echo \"$(< values-test-local.yaml)\"" > values-test-local.tmp
-set -u
+if !(helm plugin list | grep ^set); then
+  helm plugin install https://github.com/bery/helm-set.git
+fi
 
-helm --kubeconfig ${KUBECONFIG} upgrade --install ${HELM_DRY_RUN}${HELM_JOB_NAME} . -f ./values-test-local.tmp
+helm --kubeconfig ${KUBECONFIG} upgrade --install ${HELM_DRY_RUN}${HELM_JOB_NAME} . -f ./values-iste_linux.tmpl -f ./values-test-local.tmp
