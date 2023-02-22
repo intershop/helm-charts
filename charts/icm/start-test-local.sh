4311ab61ca2ad146e5137fe7486870c609e54b88
Karsten Ludwig Hauser <KHauser@intershop.com>
2023-02-22 10:41:54 +0100

    fix (icm): fix local start script (#195)

--- charts/icm/start-test-local.sh
+++ charts/icm/start-test-local.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-set -eu pipefail
+set -xeu pipefail
 
+set -o allexport
 source start-test-local_vars.sh
+set +o allexport
 
-if !(helm plugin list | grep ^set); then
-  helm plugin install https://github.com/bery/helm-set.git
-fi
+# puts envs into a comma separated list with dollar signs
+env_list=$(env | cut -d '=' -f 1 | sed 's/^/\$/;s/$/,/' | tr -d '\n' | sed 's/,$//')
 
-helm --kubeconfig ${KUBECONFIG} upgrade --install ${HELM_DRY_RUN}${HELM_JOB_NAME} . -f ./values-iste_linux.yaml -f ./values-test-local.tmp
+# now only env variables are replaced in values template
+envsubst "${env_list}" < ./values-iste_linux.tmpl | tee values-iste_linux.yaml
+
+helm --kubeconfig ${KUBECONFIG} upgrade --install ${HELM_DRY_RUN}${HELM_JOB_NAME} . -f ./values-iste_linux.yaml -f ./values-test-local.yaml
