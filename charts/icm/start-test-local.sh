e4cf05fe0da2d1248b719f6d0cf64232dc78e821
Karsten Ludwig Hauser <khauser@users.noreply.github.com>
2023-03-16 13:02:07 +0100

    * fix (icm): testrunner is now executed with intershop-user (#220)
    * fix (icm): webadapter failed with exit code 137 (#220)

--- charts/icm/start-test-local.sh
+++ charts/icm/start-test-local.sh
@@ -7,7 +7,7 @@ source start-test-local_vars.sh
 set +o allexport
 
 # puts envs into a comma separated list with dollar signs
-env_list=$(env | cut -d '=' -f 1 | sed 's/^/\$/;s/$/,/' | tr -d '\n' | sed 's/,$//')
+env_list=$(env | cut -d '=' -f 1 | sed 's/^PATH$//' | sed 's/^/\$/;s/$/,/' | tr -d '\n' | sed 's/,$//')
 
 # now only env variables are replaced in values template
 envsubst "${env_list}" < ./values-iste_linux.tmpl | tee values-iste_linux.yaml
