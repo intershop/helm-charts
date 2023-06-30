0cd43379ce3e364465e4b9fb6b06d267b0e23075
Karsten Ludwig Hauser <KHauser@intershop.com>
2023-06-30 16:19:50 +0200

    feat (icm): reenable icm-replication chart (#339)

---
+++ charts/icm-replication/start-test-local_vars.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+HELM_DRY_RUN=""
+
+SERVER_DIRECTORY="not_used"
+CONFIG_DIRECTORY="/data/testplans/1/testsuites/1/workspace/helm/iste-icm-11/config"
+RESULT_DIRECTORY="/data/testplans/1/testsuites/1/workspace/helm/iste-icm-11/result"
+HELM_DIRECTORY="/data/testplans/1/testsuites/1/workspace/helm/iste-icm-11"
+WORKSPACE_DIRECTORY="/data/testplans/1/testsuites/1/workspace"
+
+NODENAME="true"
+
+ICM_WEBSERVER_IMAGE="intershophub/icm-webadapter:2.4.6"
+ICM_WEBADAPTER_AGENT_IMAGE="intershophub/icm-webadapteragent:4.0.0"
+SERVER_DIRECTORY_GROUP="intershop"
+SERVER_DIRECTORY_USER="intershop"
