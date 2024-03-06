26c064301005bdd4311d1ace70c5dce3887bbd54
Karsten Ludwig Hauser <KHauser@intershop.com>
2024-03-06 09:23:50 +0100

    feat(icm): enable external automated test execution (#563)

--- charts/icm/start-test-local.sh
+++ charts/icm/start-test-local.sh
@@ -5,10 +5,14 @@ set -eu pipefail
 set -o allexport
 source start-test-local_vars.sh
 read -e -p 'Helm chart name: ' -i 'icm-11-test' HELM_JOB_NAME
-read -e -p 'Testsuite: ' -i 'tests.remote.com.intershop.cms.suite.PageListingTestSuite' TESTSUITE
-read -e -p 'Test image: ' -i 'intershophub/icm-as-test:11.7.0' ICM_TEST_IMAGE
+if [ -z "$TESTSUITE" ]; then
+  read -e -p 'Testsuite: ' -i 'tests.remote.com.intershop.cms.suite.PageListingTestSuite' TESTSUITE
+fi
+if [ -z "$ICM_TEST_IMAGE" ]; then
+  read -e -p 'Test image: ' -i 'intershophub/icm-as-test:11.7.0' ICM_TEST_IMAGE
+fi
 read -e -p 'Base path of your local folder mount: ' -i '/run/desktop/mnt/host/d/tmp/pv' LOCAL_MOUNT_BASE
-read -e -p 'The pull secret for the icm-as+testrunner image (e.g. dockerhub or icmbuildsnapshot): ' -i 'dockerhub' ICM_AS_PULL_SECRET
+read -e -p 'The pull secret for the icm-as+testrunner image (e.g. dockerhub or icmbuildsnapshot): ' -i 'icmbuildsnapshot' ICM_AS_PULL_SECRET
 read -e -p 'The pull secret for the icm-wa+waa image: ' -i 'dockerhub' ICM_WEB_PULL_SECRET
 set +o allexport
 
@@ -20,6 +24,4 @@ envsubst "${env_list}" < ./values-iste_linux.tmpl | tee values-iste_linux.yaml
 
 envsubst "${env_list}" < ./values-test-local.tmpl | tee values-test-local.yaml
 
-helm dependency update ../icm-as # wait for https://github.com/helm/helm/issues/2247
-helm dependency update .
 helm upgrade --install ${HELM_DRY_RUN}${HELM_JOB_NAME} . -f ./values-iste_linux.yaml -f ./values-test-local.yaml
