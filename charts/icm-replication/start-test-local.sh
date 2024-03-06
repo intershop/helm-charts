50909cbb1bef6d487919deacb4472ce56cd0438a
Karsten Ludwig Hauser <khauser@users.noreply.github.com>
2024-03-06 13:19:53 +0100

    Feature/icm/563 enable automated test execution (#570)

--- charts/icm-replication/start-test-local.sh
+++ charts/icm-replication/start-test-local.sh
@@ -6,9 +6,9 @@ set -o allexport
 source start-test-local_vars.sh
 read -e -p 'Helm chart name: ' -i 'icm-11-replication-test' HELM_JOB_NAME
 read -e -p 'Testsuite: ' -i 'tests.remote.com.intershop.product.suite.ProductMassDataReplicationTestSuite' TESTSUITE
-read -e -p 'Test image: ' -i 'intershophub/icm-as-test:11.2.0' ICM_TEST_IMAGE
+read -e -p 'Test image: ' -i 'intershophub/icm-as-test:11.9.0' ICM_TEST_IMAGE
 read -e -p 'Base path of your local folder mount: ' -i '/run/desktop/mnt/host/d/tmp/pv' LOCAL_MOUNT_BASE
-read -e -p 'The pull secret for the icm-as+testrunner image (e.g. dockerhub or icmbuildsnapshot): ' -i 'dockerhub' ICM_AS_PULL_SECRET
+read -e -p 'The pull secret for the icm-as+testrunner image (e.g. dockerhub or icmbuildsnapshot): ' -i 'icmbuildsnapshot' ICM_AS_PULL_SECRET
 read -e -p 'The pull secret for the icm-wa+waa image: ' -i 'dockerhub' ICM_WEB_PULL_SECRET
 set +o allexport
 
@@ -20,7 +20,7 @@ envsubst "${env_list}" < ./values-iste_linux.tmpl | tee values-iste_linux.yaml
 
 envsubst "${env_list}" < ./values-test-local.tmpl | tee values-test-local.yaml
 
-helm dependency update ../icm-as # wait for https://github.com/helm/helm/issues/2247
-helm dependency update ../icm
-helm dependency update .
+# create needed folders
+source start-test-local_dir-create.sh
+
 helm upgrade --install ${HELM_DRY_RUN}${HELM_JOB_NAME} . -f ./values-iste_linux.yaml -f ./values-test-local.yaml
